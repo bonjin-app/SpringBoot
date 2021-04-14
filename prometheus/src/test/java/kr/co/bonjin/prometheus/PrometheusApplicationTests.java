a67055b7e0db10426ddd2fe749611392676d8f82
bonjin-all <bonjin.app@gmail.com>
2021-04-14 11:54:40 +0900

    Add init

---
+++ prometheus/src/test/java/kr/co/bonjin/prometheus/PrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bonjin.prometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
