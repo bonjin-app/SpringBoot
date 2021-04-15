4eac2b7f2d01e6f151cc25e3cfc257beb6e93cc6
bonjin-all <bonjin.app@gmail.com>
2021-04-15 16:09:42 +0900

    Add Controller + TestController   - Logger Test

---
+++ elk/src/main/java/kr/co/bonjin/elk/TestController.java
@@ -0,0 +1,23 @@
+package kr.co.bonjin.elk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "/test")
+    public String test() {
+        logger.trace("Trace Level 테스트");
+        logger.debug("DEBUG Level 테스트");
+        logger.info("INFO Level 테스트");
+        logger.warn("Warn Level 테스트");
+        logger.error("ERROR Level 테스트");
+
+        return "test";
+    }
+}
