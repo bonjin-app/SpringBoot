ada08788865ff13fe4771dc079c87206d3c056b2
bonjin-all <bonjin.app@gmail.com>
2021-05-14 14:46:59 +0900

    init project

--- fcm/src/main/java/kr/co/bonjin/fcm/FcmApplication.java
+++ fcm/src/main/java/kr/co/bonjin/fcm/FcmApplication.java
@@ -3,11 +3,15 @@ package kr.co.bonjin.fcm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class FcmApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(FcmApplication.class, args);
+
+        Messaging.message(args);
     }
 
 }
