d17ebe359ffd533028f7e490c12dd432f8841bbe
bonjin-all <bonjin.app@gmail.com>
2021-06-14 18:05:02 +0900

    Add SlackMessage: Blocks 설정 코드 추가.

---
+++ slackhook/src/main/java/kr/co/bonjin/slackhook/model/SlackMessage.java
@@ -0,0 +1,41 @@
+package kr.co.bonjin.slackhook.model;
+
+import com.slack.api.model.block.Blocks;
+import com.slack.api.model.block.LayoutBlock;
+import com.slack.api.model.block.composition.BlockCompositions;
+import com.slack.api.model.block.element.BlockElements;
+
+public class SlackMessage {
+
+    public static LayoutBlock getHeader(String text) {
+        return Blocks.header(h -> h.text(
+                BlockCompositions.plainText(pt ->
+                        pt.emoji(true)
+                                .text(text)
+                )
+        ));
+    }
+
+    public static LayoutBlock getSection(String text) {
+        return Blocks.section(s ->
+                s.text(
+                    BlockCompositions.markdownText(text)
+                )
+        );
+    }
+
+    public static LayoutBlock getSection(String text, String imageUrl, String altText) {
+        return Blocks.section(s ->
+                s.text(
+                        BlockCompositions.markdownText(text)
+                ).accessory(BlockElements.imageElement(i ->
+                        i.imageUrl(imageUrl)
+                        .altText(altText)
+                ))
+        );
+    }
+
+    public static LayoutBlock getDivider() {
+        return Blocks.divider();
+    }
+}
--- slackhook/src/main/java/kr/co/bonjin/slackhook/services/SlackServiceImpl.java
+++ slackhook/src/main/java/kr/co/bonjin/slackhook/services/SlackServiceImpl.java
@@ -1,8 +1,10 @@
 package kr.co.bonjin.slackhook.services;
 
 import com.slack.api.Slack;
+import com.slack.api.model.block.LayoutBlock;
 import com.slack.api.webhook.Payload;
 import com.slack.api.webhook.WebhookResponse;
+import kr.co.bonjin.slackhook.model.SlackMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SlackServiceImpl implements SlackService {
@@ -34,15 +38,31 @@ public class SlackServiceImpl implements SlackService {
             // create slack message
             Slack slack = Slack.getInstance();
 
+            // Blocks Config
+            List<LayoutBlock> blockList = new ArrayList<>();
+            blockList.add(SlackMessage.getHeader(":exclamation: An error has occurred :exclamation:"));
+            blockList.add(SlackMessage.getDivider());
+            blockList.add(SlackMessage.getSection("*Error*"));
+            blockList.add(SlackMessage.getSection(
+                    "*Type:* 404 Error\n*When:* Aug 10-Aug 13\n*Hours:* 16.0 (2 days)\n*Comments:* \"Page Not Found\"",
+                    "https://miro.medium.com/max/800/1*hFwwQAW45673VGKrMPE2qQ.png",
+                    "computer thumbnail"));
+            blockList.add(SlackMessage.getDivider());
+
             Payload payload = Payload.builder()
-                    .text(text)
-                    .username("Slack WebHook Bot")  // default: 웹 설정값
-                    .iconUrl("https://bonjin.co.kr/bonjin/images/sample.png")  // default: 웹 설정값
-//                  .iconEmoji(":ghost:")
+                    .blocks(blockList)
                     .build();
 
+            // Non Blocks
+//            Payload payload = Payload.builder()
+//                    .text(text)
+//                    .username("Slack WebHook Bot")  // default: 웹 설정값
+//                    .iconUrl("https://bonjin.co.kr/bonjin/images/sample.png")  // default: 웹 설정값
+////                  .iconEmoji(":ghost:")
+//                    .build();
+
             WebhookResponse response = slack.send(WEBHOOK_URL, payload);
-            System.out.println(response);
+            logger.debug("Webhook Response :{}", response);
 
         } catch (IOException e) {
             e.printStackTrace();
