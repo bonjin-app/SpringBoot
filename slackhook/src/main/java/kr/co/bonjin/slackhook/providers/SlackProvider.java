28b5b40d4ef65433f463984f38e8bab76c0a0201
bonjin-all <bonjin.app@gmail.com>
2021-06-14 11:56:06 +0900

    Update Project: 불필요 폴더 파일 제거.

--- slackhook/src/main/java/kr/co/bonjin/slackhook/providers/SlackProvider.java
+++
@@ -1,6 +0,0 @@
-package kr.co.bonjin.slackhook.providers;
-
-public class SlackProvider {
-
-
-}
--- slackhook/src/main/java/kr/co/bonjin/slackhook/services/SlackService.java
+++ slackhook/src/main/java/kr/co/bonjin/slackhook/services/SlackService.java
@@ -2,8 +2,6 @@ package kr.co.bonjin.slackhook.services;
 
 import kr.co.bonjin.slackhook.enums.SlackMessageLevel;
 
-import java.util.Map;
-
 public interface SlackService {
     void sendMessage(SlackMessageLevel messageLevel);
 }
