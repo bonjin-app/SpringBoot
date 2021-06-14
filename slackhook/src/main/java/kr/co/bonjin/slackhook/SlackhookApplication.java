60d19f49c9b62ed387aa48ba2b997d69d3ee58e0
bonjin-all <bonjin.app@gmail.com>
2021-06-14 10:30:35 +0900

    Init SlackHook Project

---
+++ slackhook/src/main/java/kr/co/bonjin/slackhook/SlackhookApplication.java
@@ -0,0 +1,13 @@
+package kr.co.bonjin.slackhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackhookApplication.class, args);
+    }
+
+}
---
+++ slackhook/src/test/java/kr/co/bonjin/slackhook/SlackhookApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bonjin.slackhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackhookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
