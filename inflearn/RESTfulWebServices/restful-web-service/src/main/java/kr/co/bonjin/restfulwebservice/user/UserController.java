7af46fa916d38c617abe6c0d912cae30db80da2c
bonjin-all <bonjin.app@gmail.com>
2021-07-29 11:39:18 +0900

    HTTP Status Code 제어.

--- inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/UserController.java
+++ inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/UserController.java
@@ -1,9 +1,10 @@
 package kr.co.bonjin.restfulwebservice.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,5 +24,17 @@ public class UserController {
     @GetMapping(path = "/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping(path = "/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
