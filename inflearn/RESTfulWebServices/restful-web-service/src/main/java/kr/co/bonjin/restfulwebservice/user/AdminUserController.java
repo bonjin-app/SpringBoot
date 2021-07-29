5db299f557c5514719511a2b7bfd69df30d535d9
bonjin-all <bonjin.app@gmail.com>
2021-07-29 15:08:43 +0900

    Request Parameter와 Header를 이용한 API Version 관리

--- inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/AdminUserController.java
+++ inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/AdminUserController.java
@@ -33,7 +33,10 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping(path = "/v1/users/{id}")
+//    @GetMapping(path = "/v1/users/{id}")
+//    @GetMapping(value = "/users/{id}/", params = "version=1")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = service.findOne(id);
 
@@ -51,7 +54,10 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping(path = "/v2/users/{id}")
+//    @GetMapping(path = "/v2/users/{id}")
+//    @GetMapping(value = "/users/{id}/", params = "version=2")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
         User user = service.findOne(id);
 
