193330830444615020ab2ed68aa385420d1f5f12
bonjin-all <bonjin.app@gmail.com>
2021-07-29 17:37:21 +0900

    Spring Data JPA를 이용한 Entity 설정과 초기 데이터 생성.

--- inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/User.java
+++ inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/User.java
@@ -7,6 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -16,9 +20,12 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
 
-    private Integer id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
     @Size(min = 2, message = "Name은 2글자 이상을 입력해 주세요.")
     @ApiModelProperty(notes = "사용자 이름을 입력해주세요.")
--- inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/UserDaoService.java
+++ inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/UserDaoService.java
@@ -14,9 +14,9 @@ public class UserDaoService {
     private static int usersCount = 0;
 
     static {
-        users.add(new User(1, "Kenneth", new Date(), "pass1", "701010-1111111"));
-        users.add(new User(2, "Alice", new Date(), "pass2", "701010-2222222"));
-        users.add(new User(3, "Elena", new Date(), "pass3", "701010-3333333"));
+        users.add(new User(1L, "Kenneth", new Date(), "pass1", "701010-1111111"));
+        users.add(new User(2L, "Alice", new Date(), "pass2", "701010-2222222"));
+        users.add(new User(3L, "Elena", new Date(), "pass3", "701010-3333333"));
 
         usersCount = users.size();
     }
@@ -36,7 +36,7 @@ public class UserDaoService {
 
     public User save(User user) {
         if (user.getId() == null) {
-            user.setId(++usersCount);
+            user.setId((long) ++usersCount);
         }
         users.add(user);
         return user;
