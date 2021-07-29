1975d7a83efa91622a1e9e1d62ad7e4fc4f0c2db
bonjin-all <bonjin.app@gmail.com>
2021-07-29 17:23:01 +0900

    JPA 사용을 위한 Dependency 추가와 설정 .

--- inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/config/SecurityConfig.java
+++ inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package kr.co.bonjin.restfulwebservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -13,5 +14,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("gigas")
                 .password("{noop}text1234")
                 .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll();
+
+        http.csrf().disable();
+
+        http.headers().frameOptions().disable();
     }
 }
