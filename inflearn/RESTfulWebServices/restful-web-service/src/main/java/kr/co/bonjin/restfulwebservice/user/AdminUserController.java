4a58f275d6d79ef3acba776b209d844cbe091918
bonjin-all <bonjin.app@gmail.com>
2021-07-29 14:16:56 +0900

    프로그래밍으로 제어하는 Filtering 방법 - 개별 사용자 조회

---
+++ inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/AdminUserController.java
@@ -0,0 +1,43 @@
+package kr.co.bonjin.restfulwebservice.user;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/admin")
+public class AdminUserController {
+
+    private UserDaoService service;
+
+    public AdminUserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public MappingJacksonValue retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filterProvider);
+
+        return mapping;
+    }
+}
--- inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/User.java
+++ inflearn/RESTfulWebServices/restful-web-service/src/main/java/kr/co/bonjin/restfulwebservice/user/User.java
@@ -1,7 +1,6 @@
 package kr.co.bonjin.restfulwebservice.user;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,7 +10,7 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
-@JsonIgnoreProperties(value = {"password"})
+@JsonFilter("UserInfo")
 public class User {
 
     private Integer id;
@@ -22,9 +21,7 @@ public class User {
     @Past
     private Date joinDate;
 
-//    @JsonIgnore
     private String password;
 
-//    @JsonIgnore
     private String ssn;
 }
