d5eb289ce39210bf63dddc5e4d6b4a27fe13405d
bonjin <bonjin.app@gmail.com>
2023-01-13 14:10:42 +0900

    Sync Project

---
+++ jsoup/src/main/kotlin/kr/co/bonjin/jsoup/JsoupApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.bonjin.jsoup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JsoupApplication
+
+fun main(args: Array<String>) {
+    runApplication<JsoupApplication>(*args)
+}
---
+++ jsoup/src/test/kotlin/kr/co/bonjin/jsoup/JsoupApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.bonjin.jsoup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JsoupApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
